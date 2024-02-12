13a6c71cf4ee693e3994773b55e80a9966fec610
Nabugh144 <119404382+Nabugh144@users.noreply.github.com>
2024-02-12 13:50:18 +0100

    New Project

---
+++ Puissance_4/Puissance_4_indev/Grid.py
@@ -0,0 +1,14 @@
+import pyxel
+
+class Grid ():
+    
+    def __init__(self, dimensions : tuple) -> None:
+        self.width, self.height = dimensions
+        self.grid = [[None for i in range(self.width)] for j in range(self.height)]
+        pyxel.load (0,0,"Ressources\plateau.png")
+
+    def clear(self) -> None :
+        self.grid = [[None for i in range(self.width)] for j in range(self.height)]
+    
+    def insert(self, coordinates, token):
+        token.x, token.y = coordinates
---
+++ Puissance_4/Puissance_4_indev/Token.py
@@ -0,0 +1,8 @@
+import pyxel
+
+class Token():
+
+    def __init__(self, color, coordinates) -> None:
+        self.color = color
+        self.x, self.y = coordinates
+        pyxel.load(self.x, self.y, "Ressources\plateau.png")
