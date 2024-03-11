08e44ff5adbb52bcd0b48b0f69305585da1f695f
Nabugh144 <119404382+Nabugh144@users.noreply.github.com>
2024-03-11 13:17:46 +0100

    détection des touches

---
+++ Puissance_4/Puissance_4_indev/main.py
@@ -0,0 +1,37 @@
+import pyxel
+from Grid import Grid
+from Token import Token
+
+class Jeu():
+    
+    def __init__(self) -> None:
+        pyxel.init(692, 692, title="Puissance 4", fps=60)
+        self.grid = Grid()
+        self.current_color = "Jaune"
+        self.token = Token(self.current_color, (0,0))
+
+        self.run(self.update, self.draw)
+
+    def update(self) -> None:
+        pass
+
+    def draw(self) -> None:
+        pyxel.cls(0)
+        self.grid.draw()
+
+    def key_pressed(self):
+        if pyxel.KEY_RIGHT :
+            if self.token.x != 6:
+                self.token.x += 1
+        if pyxel.KEY_LEFT :
+            if self.token.x != 0 :
+                self.token.x -= 1
+        if pyxel.KEY_KP_ENTER :
+            
+            # le pion se place sur la dernière ligne
+            
+            if self.current_color == "Jaune" :
+                self.current_color = "Rouge"
+            else : self.current_color = "Jaune"
+
+Jeu()
